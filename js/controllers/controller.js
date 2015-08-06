59f0b352b76a454610c544b5bac3038d50c74153
JAMES <2008go@gmail.com>
2015-08-06 16:06:12 +0800

    some changes

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -2,24 +2,36 @@
  * Created by JAMES on 07/29/15.
  */
 
+angular.module('jpApp',[])
+    .controller('BooksController',function($scope,$http){
+        $http({
+            method: 'GET',
+            url: '/books.json'
+        }).success(function(response, status, headers, config){
+            $scope.books = response;
+        });
 
-var BooksController = function($scope, $http){
-    $http({
-        method: 'GET',
-        url: 'http://localhost:63342/myjiangpu-web/books.json'
-    }).success(function(response, status, headers, config){
-        $scope.books = response;
-    });
-}
-
-
-function BookController($scope, $routeParams, Books) {
-    $scope.book = Books.get({bookId: $routeParams.bookId},
-        function(book) {
-            $scope.bookId = book.bookId;
-            $scope.title = book.title;
-            $scope.images = book.images;
     });
 
-}
+
+//
+//var BooksController = function($scope, $http){
+//    $http({
+//        method: 'GET',
+//        url: 'http://localhost:63342/myjiangpu-web/books.json'
+//    }).success(function(response, status, headers, config){
+//        $scope.books = response;
+//    });
+//}
+//
+//
+//function BookController($scope, $routeParams, Books) {
+//
+//    $scope.book = Books.get({bookId: $routeParams.bookId},
+//        function(book) {
+//            $scope.bookId = book.bookId;
+//            $scope.title = book.title;
+//            $scope.images = book.images;
+//        });
+//}
 
